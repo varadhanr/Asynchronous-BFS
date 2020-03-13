0b43f45b1f98300f8ab8636787c335489995a345
VARADHAN <varadhan1995nitt@gmail.com>
2020-03-13 15:45:57 -0500

    Gradle initial setup

---
+++ src/main/java/AsyncBFS/MainClass.java
@@ -0,0 +1,11 @@
+package AsyncBFS;
+
+public class MainClass {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MainClass().getGreeting());
+    }
+}
